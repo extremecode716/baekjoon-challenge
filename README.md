ee299d416d2f869b6c6556c4e0bbed9f3ff397f3
ExtremeCode <extremecode716@gmail.com>
2022-02-15 23:26:07 +0900

    문제풀이 기본 구조

---
+++ README.md
@@ -0,0 +1,7 @@
+# 백준 코딩 테스트 (extreme code)
+
+- 패키지 번호가 백준 문제 번호 입니다.
+- 백준은 Main 형태로 코테를 진행합니다.
+- Algorithm 클래스와 ExtremeCodeTest 큰 틀로 문제풀이를 진행합니다.
+
+## Game Start
---
+++ src/main/java/baekjoon/Algorithm.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 알고리즘 풀이 구조 클래스<p>
+ * 백준에 제출하기 위해 해당 클래스를 가져가서 사용합니다.
+ *
+ * @author extreme code
+ */
+class Algorithm {
+    @FunctionalInterface
+    public interface Solution {
+        void solve();
+    }
+
+    public static List<String> getInputs(final int lineCount) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in))) {
+            int count = 0;
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                lines.add(line);
+                ++count;
+                if (count == lineCount)
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void solve(Solution solution) {
+        solution.solve();
+    }
+}

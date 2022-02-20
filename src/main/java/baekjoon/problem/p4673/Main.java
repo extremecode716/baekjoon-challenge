042821ad6638ebb69c5b7be106e16379ad8b7d0a
ExtremeCode <extremecode716@gmail.com>
2022-02-20 22:25:06 +0900

    4673

---
+++ src/main/java/baekjoon/problem/p4673/Main.java
@@ -0,0 +1,107 @@
+package baekjoon.problem.p4673;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 셀프 넘버
+ *
+ * @author extreme code
+ * @see <a href="https://www.acmicpc.net/problem/4673">https://www.acmicpc.net/problem/4673</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        Algorithm.solve(Main::solution);
+    }
+
+    private static void solution() {
+        final int maxNumber = 10000;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int n = 1; n <= maxNumber; ++n) {
+            if (isSelfNumber(n))
+                stringBuilder.append(n).append("\n");
+        }
+        System.out.print(stringBuilder);
+    }
+
+    private static int d(int n) {
+        int sum = n;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    private static boolean isSelfNumber(int n) {
+        for (int i = 1; i <= n; ++i) {
+            if (d(i) == n)
+                return false;
+        }
+        return true;
+    }
+}
+
+/**
+ * 백준에 제출하기 위해 문제의 Main 클래스로 복사 이동하여 사용됩니다.
+ *
+ * @author extreme code
+ */
+class Algorithm {
+    @FunctionalInterface
+    public interface Solution {
+        void solve();
+    }
+
+    private static BufferedReader bufferedReader;
+
+    public static String readLine() {
+        bufferedReader = bufferedReader == null ? new BufferedReader(new InputStreamReader(System.in)) : bufferedReader;
+        String line = null;
+        try {
+            line = bufferedReader.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+
+    public static List<String> readLines(final int readCount) {
+        bufferedReader = bufferedReader == null ? new BufferedReader(new InputStreamReader(System.in)) : bufferedReader;
+        List<String> lines = new ArrayList<>();
+        try {
+            int count = 0;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+                ++count;
+                if (count == readCount)
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void close() {
+        if (null == bufferedReader)
+            return;
+        try {
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            bufferedReader = null;
+        }
+    }
+
+    public static void solve(Solution solution) {
+        solution.solve();
+        close();
+    }
+}
